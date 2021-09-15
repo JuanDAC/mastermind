a302283bf599fccabf4546bcc58337d7117c4d48
JuanDAC <juanavila12.0308@gmail.com>
2021-09-15 07:21:00 -0400

    [set-up] (add global styles)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
+import styles from './app.css';
 import { GameCanvas } from './components/gui/game_canvas.js';
 
+
+const globalStyles =  document.createElement('style');
+globalStyles.innerHTML = styles.toString();
+document.head.appendChild(globalStyles);
 
 
 window.customElements.define('game-canvas', GameCanvas);
