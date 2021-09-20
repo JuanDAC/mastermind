3323a599022ae1e80c7cd9ee9ed17ce6aece0551
jepez90 <ing.jersonperez@gmail.com>
2021-09-19 22:25:56 -0500

    [Update] handle errors

--- api/helper.py
+++ api/helper.py
@@ -4,31 +4,28 @@ import json
 def make_response(data=None):
     """ """
     from api.index import app
-    code = 200
-    if data is None:
-        data = {'error': 'Invalid Request'}
-        code = 404
+
     return app.response_class(
         response=json.dumps(data),
-        status=code,
+        status=200,
         mimetype='application/json'
     )
 
 
 def get_start_args(request):
+    session_id = request.args.get('key', type=str)
     mode = request.args.get('mode', default=1, type=int)
     rows = request.args.get('rows', default=4, type=int)
     max_colors = request.args.get('max_colors', default=8, type=int)
-    player_name = request.args.get('player', type=str)
+    player_name = request.args.get('player_name', type=str)
     rounds = request.args.get('rounds', default=10, type=int)
-    session_id = request.args.get('key', type=str)
 
     return mode, rows, max_colors, player_name, rounds, session_id
 
 
 def get_move_args(request):
-    id = request.args.get('id', type=str)
     key = request.args.get('key', type=str)
+    id = request.args.get('game_id', type=str)
     move = request.args.get('move', type=str)
 
     return id, move, key
--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
 """ """
+import json
+from flask import abort
 from flask import Flask
 from flask import request
 import uuid
@@ -9,7 +11,7 @@ from api.helper import get_move_args
 from api.helper import st_get, st_save
 
 
-db={}
+db = {}
 app = Flask(__name__)
 
 
@@ -17,17 +19,18 @@ app = Flask(__name__)
 def start():
     """ """
     # 1. validate parameters
-    mode, rows, max_colors, player_name, max_rounds, session_id = get_start_args(request)
+    mode, rows, max_colors, player_name, max_rounds, session_id\
+        = get_start_args(request)
 
     if rows > 7 or rows < 1 or max_rounds > 50 or max_colors < rows:
-        return make_response()
+        abort(400, "Invalid Params")
 
     # 2. create id and validate player_name
     newId = str(uuid.uuid4())
     if player_name is None:
         player_name = 'Player-' + newId[0: 13]
     elif len(player_name) > 20:
-        return make_response()
+        abort(400, "Player name too long")
 
     # 3. Create color list and color combination
     color_list = random.sample(range(0, max_colors), rows)
@@ -39,7 +42,7 @@ def start():
     if session_id is None:
         session_id = str(uuid.uuid4())
     game = {
-        'id': newId,
+        'game_id': newId,
         'combination': combination,
         'player_name': player_name,
         'mode': mode,
@@ -48,46 +51,51 @@ def start():
     }
 
     st_save(session_id, game, db)
-    
+
     # 5. generate the response
-    data = {'id': newId, 'use_colors': color_list, 'key':session_id}
+    data = {'key': session_id, 'game_id': newId, 'use_colors': color_list}
     return make_response(data)
 
 
-@app.route('/check_move', strict_slashes=False)
+@app.route('/check', strict_slashes=False)
 def check():
     """ """
     # 1. get parameters
-    id, user_combination, key = get_move_args(request)
+    game_id, user_combination, key = get_move_args(request)
 
-    if key is None or id is None or user_combination is None or len(id) > 36:
-        return make_response()
+    if key is None:
+        abort(400, "Missing key")
+    if game_id is None:
+        abort(400, "Missing game_id")
+    if user_combination is None:
+        abort(400, "Missing move")
+    if len(game_id) > 36:
+        abort(400, "Invalid game_id")
 
     user_combination = [int(i) for i in user_combination.split(',')]
     if len(user_combination) > 7:
-        return make_response()
-    print(st_get(key, db) )
-    print(st_get(key, db, id) )
+        abort(400, "Invalid Move")
 
-    if st_get(key, db) is None or st_get(key, db, 'id') != id:
-        return make_response()
-    print("Hola Mubndo 3 ")
+    if st_get(key, db) is None or st_get(key, db, 'game_id') != game_id:
+        abort(400, "Invalid Move")
+
     # 2. get session data
     game_data = st_get(key, db)
     if (game_data is None):
-        return make_response()
+        abort(400, "Invalid key")
 
-    game_data['round'] +=1
+    game_data['round'] += 1
 
     # 5. save data in session
     st_save(key, game_data, db)
-   
+
     # 3. check status of the game
     if len(game_data['combination']) != len(user_combination):
-        return make_response()
+        abort(400, "Invalid Move")
 
     data = {'color_match': 0, 'position_match': 0, 'no_match': 0}
 
+    # finished game
     if game_data['round'] > game_data['max_rounds']:
         data['combination'] = game_data['combination']
         return make_response(data)
@@ -115,3 +123,24 @@ def check():
 
     return make_response(data)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Method to handle errors """
+    data = {"error": "Not found"}
+    return app.response_class(
+        json.dumps(data),
+        status=404,
+        mimetype='application/json'
+    )
+
+
+@ app.errorhandler(400)
+def handle_error(error):
+    """ Method to handle errors """
+    data = {"error": error.description}
+    return app.response_class(
+        json.dumps(data),
+        status=400,
+        mimetype='application/json'
+    )
