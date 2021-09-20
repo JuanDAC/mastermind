e3f534de75939bb8be82a528dcac55191f6eb2db
JuanDAC <juanavila12.0308@gmail.com>
2021-09-19 23:17:13 -0400

    [docs] (documentation of Gaphic User Interface)

--- src/components/gui/game_canvas.js
+++ src/components/gui/game_canvas.js
@@ -1,12 +1,31 @@
+/**
+ * game_canvas
+ * @module app/components/game_objects/game_canvas
+ */
+
+/** Abstracts imports . */
 import { Schema } from '../schema.js';
+/** Style imports . */
 import styles from './game_canvas.css';
 
+/**
+ * Class representing a GameCanvas.
+ * @extends Schema
+ */
 export class GameCanvas extends Schema {
+
+  /**
+   * Logic of component after rendering
+   */
   initComponent () {
     this.$canvas = this.shadowDOM.querySelector('main[canvas]');
     this.$rooms = this.shadowDOM.querySelectorAll('slot[room]');
   }
 
+  /**
+   * Defines the component HTML elements
+   * @return { string } The styles of element with wrapper.
+   */
   template () {
     const roomsIterator = ['rooms', this.getAttribute.bind(this), parseInt, Array]
       .reduce((value, funct) => funct(value)).fill();
@@ -20,6 +39,10 @@ export class GameCanvas extends Schema {
     `;
   }
 
+  /**
+   * Defines the component styles
+   * @return { string } The styles of element with wrapper.
+   */
   templateCss () {
     return `
       <style>
@@ -28,12 +51,20 @@ export class GameCanvas extends Schema {
     `;
   }
 
+  /**
+   * Maps the array of attributes.
+   * @return { [ { Key, value } ] } The object that denied an atribute.
+   */
   mapComponentAttributes () {
     return [
       { key: 'rooms', value: 0 }
     ];
   }
 
+  /**
+   * Action that receives dimensions with cumstom properties of css
+   * @return { [store, windowsResizeCallback] } The array containing the store and the action.
+   */
   actionWindowResize () {
     return ['guiStore', ({ actionType, height }) => {
       if (actionType === 'window-resize') {
--- src/components/gui/room.js
+++ src/components/gui/room.js
@@ -1,14 +1,33 @@
+/**
+ * game_canvas
+ * @module app/components/game_objects/game_canvas
+ */
+
+/** Abstracts imports . */
 import { Schema } from '../schema.js';
+/** Style imports . */
 import styles from './room.css';
+/** Files imports . */
 import { ingameOne } from '../../assets/audio/ingame_one.mp3';
 import { ingameTwo } from '../../assets/audio/ingame_two.mp3';
 
+/**
+ * Class representing a Room.
+ * @extends Schema
+ */
 export class Room extends Schema {
+
+  /**
+   * Logic of component after rendering
+   */
   constructor () {
     super();
     this.ingameAudios = [new Audio(ingameOne), new Audio(ingameTwo)];
   }
 
+  /**
+   * Logic of component after rendering
+   */
   initComponent () {
     this.$checkeds = this.shadowDOM.querySelector('#checked');
     this.$hits = this.shadowDOM.querySelector('#hit');
@@ -16,33 +35,37 @@ export class Room extends Schema {
     this.typeRoom = this.getAttribute('type');
 
     switch (this.typeRoom) {
-      case 'load':
-        break;
-      case 'home':
-        break;
-      case 'selector-avatar':
-        break;
-      case 'map':
-        break;
-      case 'game':
-        this.$hits.addEventListener('scroll', () => {
-          const { scrollLeft } = this.$hits;
-          this.$checkeds.scrollLeft = scrollLeft;
-        });
+    case 'load':
+      break;
+    case 'home':
+      break;
+    case 'selector-avatar':
+      break;
+    case 'map':
+      break;
+    case 'game':
+      this.$hits.addEventListener('scroll', () => {
+        const { scrollLeft } = this.$hits;
+        this.$checkeds.scrollLeft = scrollLeft;
+      });
 
-        this.$checkeds.addEventListener('scroll', () => {
-          const { scrollLeft } = this.$checkeds;
-          this.$hits.scrollLeft = scrollLeft;
-        });
+      this.$checkeds.addEventListener('scroll', () => {
+        const { scrollLeft } = this.$checkeds;
+        this.$hits.scrollLeft = scrollLeft;
+      });
 
-        // TODO change audio game
-        this.ingameAudios[0].play();
+      // TODO change audio game
+      this.ingameAudios[0].play();
 
-        break;
-      default:
+      break;
+    default:
     }
   }
 
+  /**
+   * Action that receives image and add in elements checkeds
+   * @return { [store, imageCheckCodeCallback] } The array containing the store and the action.
+   */
   actionImageCheckCode () {
     return ['guiStore', ({ actionType, imageCheckCode, width }) => {
       if (actionType === 'image-check-code' && this.typeRoom === 'game') {
@@ -58,41 +81,49 @@ export class Room extends Schema {
     }];
   }
 
+  /**
+   * Defines the component HTML elements
+   * @return { string } The styles of element with wrapper.
+   */
   template () {
     this.typeRoom = this.getAttribute('type');
     switch (this.typeRoom) {
-      case 'load':
-        return `
-        <div class="spinner"></div>
-      `;
-      case 'home':
-        return `
-        <slot></slot>
-      `;
-      case 'selector-avatar':
-        return `
-        <slot></slot>
-      `;
-      case 'map':
-        return `
-        <slot></slot>
-      `;
-      case 'game':
-        return `
-        <game-selector-colors></game-selector-colors>
-        <div class="cell interaction" id="insert" >
-          <game-check></game-check>
-        </div>
-        <div class="cell chekc" id="checked" ></div>
-        <div class="cell hits" id="hit" ></div>
-      `;
-      default:
-        return `
-        <h1>Attribute ${this.typeRoom} not exist<h1>
-      `;
+    case 'load':
+      return `
+      <div class="spinner"></div>
+    `;
+    case 'home':
+      return `
+      <slot></slot>
+    `;
+    case 'selector-avatar':
+      return `
+      <slot></slot>
+    `;
+    case 'map':
+      return `
+      <slot></slot>
+    `;
+    case 'game':
+      return `
+      <game-selector-colors></game-selector-colors>
+      <div class="cell interaction" id="insert" >
+        <game-check></game-check>
+      </div>
+      <div class="cell chekc" id="checked" ></div>
+      <div class="cell hits" id="hit" ></div>
+    `;
+    default:
+      return `
+      <h1>Attribute ${this.typeRoom} not exist<h1>
+    `;
     }
   }
 
+  /**
+   * Defines the component styles
+   * @return { string } The styles of element with wrapper.
+   */
   templateCss () {
     return `
       <style>
@@ -101,6 +132,10 @@ export class Room extends Schema {
     `;
   }
 
+  /**
+   * Maps the array of attributes.
+   * @return { [ { Key, value } ] } The object that denied an atribute.
+   */
   mapComponentAttributes () {
     return [
       { key: 'type', value: 'load' },
@@ -108,6 +143,10 @@ export class Room extends Schema {
     ];
   }
 
+  /**
+   * Action that receives dimensions with cumstom properties of css
+   * @return { [store, windowsResizeCallback] } The array containing the store and the action.
+   */
   actionWindowsResize () {
     return ['guiStore', ({ actionType, height, width }) => {
       if (actionType === 'window-resize') {
